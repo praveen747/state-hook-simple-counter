123dbf6bb727c5fbb0eef697d8b876abaf6efb7c
Praveen Padamati <praveenpadamati@Praveens-MacBook-Pro.local>
2021-05-13 10:46:43 -0500

    simple-counter-with-using-state-hook

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React, { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>SIMPLE COUNTER WITH STATE-HOOK</h1>
+     <button type="primary" onClick={() => setCount(count + 1)}>+</button>
+     <button type="primary" onClick={() => setCount(count - 1)}>-</button>
+     <h2>{count}</h2>
     </div>
   );
-}
-
+  }
 export default App;
